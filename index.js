eacb57565e34b8b1fcaa380b0c7803fa90232d79
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-11-01 22:48:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ document.addEventListener("DOMContentloaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
   const titanicButton = document.querySelector('#Titanic')
+  const terminatorButton = document.querySelector('#Terminator 2')
   
+  titanicButton.addEventListener('click', fillInMovieData)
+
 });
+
+function fillInMovieData() {
+  
+}
