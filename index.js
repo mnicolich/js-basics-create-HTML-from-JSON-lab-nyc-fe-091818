da80670299924128dd958f95856c6d9ae2f7fbb0
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-11-01 23:55:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,54 +4,62 @@ document.addEventListener("DOMContentLoaded", (e) => {
   const titanicButton = document.querySelector('#Titanic')
   const terminatorButton = document.querySelector('#Terminator-2')
   
-  titanicButton.addEventListener('click', () => fillInMovieData('Titanic')
-  terminatorButton.addEventListener('click', () => fillInMovieData('Terminator-2')
-
+  titanicButton.addEventListener('click', () => fillInMovieData('Titanic'))
+  terminatorButton.addEventListener('click', () => fillInMovieData('Terminator 2'))
+  
 });
 
 function fillInMovieData(movie) {
   const currentMovie = movies[movie]
   
-  
-  const title = movies['Titanic'].title
+  const title = currentMovie.title // "Titanic"
   const titleDiv = document.querySelector('#title')
   titleDiv.innerText = title
-
-  const director = currentMovie.director
-   const directorDiv = document.querySelector('#director')
- directorDiv.innerText = director
- 
- const genre = currentMovie.genre
- const genreDiv = document.querySelector('#genre')
- genreDiv.innerText = genre
-
- const filmRating = currentMovie.filmRating
- const filmRatingDiv = document.querySelector('#filmRating')
- filmRatingDiv.innerText = filmRating
- 
- const poster = currentMovie.poster
- const posterImg = document.querySelector('#poster')
-posterImg.src = poster 
-
- const description = currentMovie.description
- const descriptionDiv = document.querySelector('#description')
-descriptionDiv.innerText = description 
-
- const audienceScore = currentMovie.audienceScore
- const audienceScoreDiv = document.querySelector('#audienceScore')
-audienceScoreDive.innerText = audienceScore
-
-for (let castMember in currentMovie.cast) {
-  console.log(castMember)
-  const li = document.createElement('li')
-  li.innerText = castMember.role + ':' +  castMember.actor
-  const div = document.querySelector('#cast')
-  div.appendChild(li)
   
-  for (let review in currentMovie.reviews) {
-  const li = document.createElement('li')
-  li.innerText = review.username + ':' + review.content
-  const div = document.querySelector('#reviews')
-  div.appendChild(li)
-}
+  const director = currentMovie.director
+  const directorDiv = document.querySelector('#director')
+  directorDiv.innerText = director
+  
+  const genre = currentMovie.genre
+  const genreDiv = document.querySelector('#genre')
+  genreDiv.innerText = genre
+  
+  const filmRating = currentMovie.filmRating
+  const filmRatingDiv = document.querySelector('#filmRating')
+  filmRatingDiv.innerText = filmRating
+  
+  const poster = currentMovie.poster
+  const posterImg = document.querySelector('#poster')
+  posterImg.src = poster
+  
+  const description = currentMovie.description
+  const descriptionDiv = document.querySelector('#description')
+  descriptionDiv.innerText = description
+  
+  const audienceScore = currentMovie.audienceScore
+  const audienceScoreDiv = document.querySelector('#audienceScore')
+  audienceScoreDiv.innerText = audienceScore
+  
+  // clear out old cast members
+  const castDiv = document.querySelector('#cast')
+  while (castDiv.hasChildNodes()) {
+    castDiv.removeChild(castDiv.firstChild)
+  }
+  
+  for (let castMember of currentMovie.cast) {
+    console.log(castMember)
+    const li = document.createElement('li')
+    // <li></li>
+    li.innerText = castMember.role + ': ' + castMember.actor
+    const div = document.querySelector('#cast')
+    div.appendChild(li)
+  }
+  
+  for (let review of currentMovie.reviews) {
+    const li = document.createElement('li')
+    // <li></li>
+    li.innerText = review.username + ': ' + review.content
+    const div = document.querySelector('#reviews')
+    div.appendChild(li)
+  }
 }
