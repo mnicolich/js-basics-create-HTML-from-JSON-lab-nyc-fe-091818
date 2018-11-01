37bc20e6865c6f9b5c1994ad8b5e8803e796dcc9
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-11-01 22:47:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentloaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
+  const titanicButton = document.querySelector('#Titanic')
   
 });
